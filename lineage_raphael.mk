af70841494099fc0744c5c006e4cf74aa4b13bda
AGENT-RR <eziosimon1995@gmail.com>
2021-07-19 19:50:39 +0530

    raphael: cipher bringup

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -16,13 +16,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Lineage X stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 EXTRA_FOD_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_raphael
+PRODUCT_NAME := cipher_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
